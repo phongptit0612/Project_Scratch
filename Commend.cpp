16f4dbe32777f5115fae05cfea77d4a66260f4ff
Phong Du <ptran4109@gmail.com>
2024-12-11 15:49:22 +0700

    Add files via upload

---
+++ Commend.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    // Kieu du lieu so nguyen
+    int a = 10;  // Khai bao gia tri 10
+
+    // Kieu du lieu so thuc
+    float b = 3.14;  // Khai báo và khoi tao bien 3,14
+
+    // Kieu du lieu double
+    double c = 3.1415926535;  // Khai bao va khoi tao bien Pi (3.14...) 
+
+    // Kieu du lieu ki tu
+    char d = 'A';  // Khai bao va khoi tao ki tu A
+
+    // Kieu du lieu short
+    short e = 1000;  // Khai bao va khoi tao bien 1000
+
+    // Kieu du lieu long
+    long f = 100000L;  // Khai bao va khoi tao bien 100000L
+   
+    return 0;
+}
+
