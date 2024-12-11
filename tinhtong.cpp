fe9d83ef8dbd361c1c87b99ca9ec31fc3d292eb3
Phong Du <ptran4109@gmail.com>
2024-12-11 16:02:00 +0700

    tinhtong.cpp

--- tinhtong.cpp
+++ tinhtong.cpp
@@ -4,12 +4,15 @@ int main() {
     // Khai bao cac gia tri so nguyen
     int num1 = 5;  // Bien num1 gan gia tri = 5
     int num2 = 7;  // Bien num2 gan gia tri = 7
-
+    int sum2;
     // khai bao bien va luu ket qua tong
     int sum = num1 + num2;  // Tinh tong
-    // In kq ra ma hinh
-    printf("Tong la: %d\n", sum);
     
+    // Luu kqua saang bien khac
+    sum2 = sum;
+    // Xuat kqua ra man hinh
+    printf("Tong la: %d\n", sum2);
+
     return 0;
 }
 
