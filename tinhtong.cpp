90904f5456b538bfe1b94a735013a7abfd55f6b0
Phong Du <ptran4109@gmail.com>
2024-12-11 15:52:08 +0700

    Add files via upload

---
+++ tinhtong.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Khai bao cac gia tri so nguyen
+    int num1 = 5;  // Bien num1 gan gia tri = 5
+    int num2 = 7;  // Bien num2 gan gia tri = 7
+
+    // khai bao bien va luu ket qua tong
+    int sum = num1 + num2;  // Tinh tong
+    // In kq ra ma hinh
+    printf("Tong la: %d\n", sum);
+    
+    return 0;
+}
+
