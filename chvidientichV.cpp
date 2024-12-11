3b1c6654592fe36bf0b37dd6c5be7e803a2e8dde
Phong Du <ptran4109@gmail.com>
2024-12-11 15:57:34 +0700

    Add files via upload

---
+++ chvidientichV.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float side = 4.5;  
+    float peri = 4 * side;  
+    float area = side * side;   
+
+    printf("Chu vi hinh vuong la: %.2f\n", peri);
+    printf("Dien tich hinh vuong la: %.2f\n", area);
+
+    return 0;
+}
+
